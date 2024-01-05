b7d63c40e7ed4492ebe6debc98419b24fd192d49
Eduardo Costa <m4c0@users.noreply.github.com>
2024-01-05 15:38:05 +0000

    full decode of the code chunk of "hello.erl"

--- e2l.erl
+++ e2l.erl
@@ -18,20 +18,36 @@ parse_chunks(<<Id:4/binary, Sz:32/big, Blob/binary>>, Acc) ->
 parse_chunk("Code", <<HdSz:32/big, Hd:HdSz/binary, Code/binary>>, Acc) ->
   <<_InstSet:32/big, _OpcodeMax:32/big, _LabelCount:32/big, _FnCount:32/big, _/binary>> = Hd,
   Acc#{code => code(Code, [])};
-parse_chunk(_, _, Acc) -> Acc.
+parse_chunk("LitT", <<_UnSz:32/big, Comp/binary>>, Acc) ->
+  Acc#{literals => lit(zlib:uncompress(Comp))};
+parse_chunk(Id, Data, Acc) -> Acc#{Id => Data}.
 
-code(<<1:8/big, Data/binary>>, Acc) -> 
-  {[{literal, N}], Rest} = args(1, Data),
-  code(Rest, [{label, N}|Acc]);
-code(<<2:8/big, Data/binary>>, Acc) ->
-  {As, Rest} = args(3, Data),
-  code(Rest, [{func_info, As}|Acc]);
-code(<<153:8/big, Data/binary>>, Acc) ->
-  {[{literal, N}], Rest} = args(1, Data),
-  code(Rest, [{line, N}|Acc]);
-code(<<>>, Acc) -> lists:reverse(Acc).
+lit(<<Count:32/big, Data/binary>>) -> lit(Count, Data, []).
+lit(0, <<>>, Acc) -> lists:reverse(Acc);
+lit(N, <<Sz:32/big, Lit:Sz/binary, Rest/binary>>, Acc) -> lit(N - 1, Rest, [binary_to_term(Lit)|Acc]).
+
+code(<<1:8/big, Data/binary>>, Acc) -> opcode(label, 1, Data, Acc);
+code(<<2:8/big, Data/binary>>, Acc) -> opcode(func_info, 3, Data, Acc);
+code(<<3:8/big, Data/binary>>, Acc) -> opcode(int_code_end, 0, Data, Acc);
+code(<<64:8/big, Data/binary>>, Acc) -> opcode(move, 2, Data, Acc);
+code(<<78:8/big, Data/binary>>, Acc) -> opcode(call_ext_only, 2, Data, Acc);
+code(<<153:8/big, Data/binary>>, Acc) -> opcode(line, 1, Data, Acc);
+code(<<>>, Acc) -> lists:reverse(Acc);
+code(Rem, Acc) -> lists:reverse([{failed, Rem}|Acc]).
+
+opcode(Lbl, Arity, Data, Acc) ->
+  {Args, Rest} = args(Arity, Data),
+  code(Rest, [{Lbl, Args}|Acc]).
 
 args(Arity, Data) -> args(Arity, [], Data).
 args(0, Acc, Rest) -> {lists:reverse(Acc), Rest};
-args(N, Acc, <<L:5/big, 0:3/big, Rest/binary>>) -> args(N - 1, [{literal, L}|Acc], Rest);
-args(N, Acc, <<L:5/big, 2:3/big, Rest/binary>>) -> args(N - 1, [{atom, L}|Acc], Rest).
+args(N, Acc, <<L:4/big, 0:1/big, 0:3/big, Rest/binary>>) -> args(N - 1, [{literal, L}|Acc], Rest);
+args(N, Acc, <<L:4/big, 0:1/big, 1:3/big, Rest/binary>>) -> args(N - 1, [{int, L}|Acc], Rest);
+args(N, Acc, <<L:4/big, 0:1/big, 2:3/big, Rest/binary>>) -> args(N - 1, [{atom, L}|Acc], Rest);
+args(N, Acc, <<L:4/big, 0:1/big, 3:3/big, Rest/binary>>) -> args(N - 1, [{x, L}|Acc], Rest);
+args(N, Acc, <<L:4/big, 0:1/big, 4:3/big, Rest/binary>>) -> args(N - 1, [{y, L}|Acc], Rest);
+args(N, Acc, <<L:4/big, 0:1/big, 5:3/big, Rest/binary>>) -> args(N - 1, [{label, L}|Acc], Rest);
+args(N, Acc, <<L:4/big, 0:1/big, 6:3/big, Rest/binary>>) -> args(N - 1, [{char, L}|Acc], Rest);
+%% OTP-20 values only
+args(N, Acc, <<4:4/big, 0:1/big, 7:3/big, Sz:8/big, Lit:Sz/big, Rest/binary>>) -> 
+  args(N - 1, [{literal_ex, Lit}|Acc], Rest).
