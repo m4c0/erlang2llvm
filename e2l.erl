e4f6626ef655045dfe08e2519eacad7bcb1e68cc
Eduardo Costa <m4c0@users.noreply.github.com>
2024-01-05 11:47:12 +0000

    preparing to parse code chunk

--- e2l.erl
+++ e2l.erl
@@ -2,16 +2,20 @@
 -export([main/1]).
 
 main([Filename]) ->
-  { ok, <<"FOR1",Sz:32,Data:Sz/binary>> } = file:read_file(Filename),
+  { ok, <<"FOR1",Sz:32/big,Data:Sz/binary>> } = file:read_file(Filename),
   <<"BEAM",ChunksData/binary>> = Data,
   Chunks = parse_chunks(ChunksData),
   io:format("~p~n", [Chunks]).
 
 parse_chunks(X) -> parse_chunks(X, #{}).
-
 parse_chunks(<<>>, Acc) -> Acc;
-parse_chunks(<<Id:4/binary, Sz:32, Blob/binary>>, Acc) ->
-  PadSz = 4 * ((Sz + 3) div 4),
-  <<Data:PadSz/binary, Rest/binary>> = Blob,
-  parse_chunks(Rest, Acc#{binary_to_list(Id) => {Sz, Data}}).
+parse_chunks(<<Id:4/binary, Sz:32/big, Blob/binary>>, Acc) ->
+  PaddedSz = 4 * ((Sz + 3) div 4),
+  PadSz = PaddedSz - Sz,
+  <<Data:Sz/binary, _:PadSz/binary, Rest/binary>> = Blob,
+  parse_chunks(Rest, parse_chunk(binary_to_list(Id), Data, Acc)).
 
+parse_chunk("Code", <<HdSz:32/big, Hd:HdSz/binary, Code/binary>>, Acc) ->
+  <<_InstSet:32/big, _OpcodeMax:32/big, _LabelCount:32/big, _FnCount:32/big, _/binary>> = Hd,
+  Acc#{code => Code};
+parse_chunk(_, _, Acc) -> Acc.
